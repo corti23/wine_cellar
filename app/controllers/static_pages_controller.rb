4ff0a9dfe3d75a12ebf3601777be931759808c7a
corti23 <contact@mezzadri.fr>
2018-03-03 20:00:44 +0100

    Add localization (fix)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      redirect_to user_path(current_user)
+    else
+      @user = User.new
+    end
   end
 
   def help
@@ -8,6 +13,21 @@ class StaticPagesController < ApplicationController
   def about
   end
   
-   def contact
+  def contact
   end
+   
+   def locale
+    case params[:locale]
+    when 'fr'
+      I18n.locale = 'fr'
+    else
+      I18n.locale = 'en'
+    end
+    
+    cookies.permanent[:locale]=I18n.locale    
+    redirect_back(fallback_location: root_path)
+
+  end
+  
+  
 end
