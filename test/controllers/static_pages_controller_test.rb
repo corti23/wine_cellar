d56235c738dda1a843a30e178963e5f8affe3b51
corti23 <contact@mezzadri.fr>
2018-03-03 14:53:07 +0100

    Add localization

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Your Wine Cellar"
   end
 
   test "should get home" do
