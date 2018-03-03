9c88cfc957126b2c2d31ad329416ef1e6ee699cc
corti23 <contact@mezzadri.fr>
2018-03-03 14:51:06 +0100

    Add localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,28 @@ class ApplicationController < ActionController::Base
   private
    
   def set_locale
-    I18n.locale = extract_locale || I18n.default_locale
+    #I18n.locale = extract_locale || I18n.default_locale
+    #I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = cookies[:locale] || extract_locale_from_accept_language_header unless Rails.env.test?
+    logger.debug "* Locale set to '#{I18n.locale}'"
   end
    
   def extract_locale
     parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+  
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+    
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
   
